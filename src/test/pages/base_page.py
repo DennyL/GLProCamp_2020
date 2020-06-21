16bc1ddecc4daddb4b3d9feb211bc18170f8a860
DennyL <denys@jarvis.Jarvis>
2020-06-21 15:28:06 +0300

    In the base_page.py, added an altered __init__ method to connect to a remote computer running Selenium server, and to run the test remotedly. Implemented scallable run_browser_from_stack method that enables the remote machine browser selection

--- src/test/pages/base_page.py
+++ src/test/pages/base_page.py
@@ -1,15 +1,38 @@
 from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.opera import OperaDriverManager
 
 
 class BasePage:
+
     """ The base object that contains common to all the page objects in the app """
-    def __init__(self, with_browser='Chrome'):
-        if with_browser == 'Chrome':
-            self.driver = webdriver.Chrome(ChromeDriverManager().install())
-        elif with_browser == 'Opera':
-            self.driver = webdriver.Opera(executable_path=OperaDriverManager().install())
+
+    ##### INIT FOR LOCAL RUNS #####
+
+    # def __init__(self, with_browser='chrome'):
+    #     if with_browser == 'chrome':
+    #         self.driver = webdriver.Chrome(ChromeDriverManager().install())
+    #     elif with_browser == 'opera':
+    #         self.driver = webdriver.Opera(executable_path=OperaDriverManager().install())
+    #     else:
+    #         raise Exception('Browser name is not defined')
+
+
+    ##### INIT FOR REMOTE RUNS #####
+
+    def __init__(self, with_browser='chrome'):
+        self.driver = webdriver.Remote(
+            command_executor='http://192.168.1.103:4444/wd/hub',
+            desired_capabilities=self.run_browser_from_stack(with_browser)
+        )
+
+    @staticmethod
+    def run_browser_from_stack(browser_name):
+        if browser_name.lower() == 'chrome':
+            return DesiredCapabilities.CHROME
+        elif browser_name.lower() == 'opera':
+            return DesiredCapabilities.OPERA
         else:
             raise Exception('Browser name is not defined')
 
