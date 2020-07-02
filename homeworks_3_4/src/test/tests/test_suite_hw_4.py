84bb95a43cf7f16cf1433540bd9b364e30856c40
DennyL <denis.lozinskij@gmail.com>
2020-07-02 23:32:59 +0300

    split the test from hw4 into 2 separate ones: one verifies adding items to a cart, another one - their removal

--- homeworks_3_4/src/test/tests/test_suite_hw_4.py
+++ homeworks_3_4/src/test/tests/test_suite_hw_4.py
@@ -2,7 +2,7 @@ import pytest
 from homeworks_3_4.src.test.pages.main_page import MainPage
 
 
-@pytest.fixture
+@pytest.fixture(scope='session')
 def main_page(browser):
     page = MainPage(browser)
     page.open()
@@ -10,8 +10,8 @@ def main_page(browser):
 
 
 def test_add_items_to_the_cart(main_page):
-    """ verifies if items can be added to a cart
-        and the qty number on the art badge increases by one as a new item added
+    """ verifies if items can be added to a cart,
+        and the qty number on the cart badge increases by one as a new item added
     """
     for item in main_page.popular_products_locators(qty=3):
         main_page.click_on(item)
@@ -19,6 +19,13 @@ def test_add_items_to_the_cart(main_page):
         # that returns True if the qty number on the badge increased by 1 after the click, or False otherwise
         assert main_page.is_cart_qty_badge_number_grew(main_page.click_on)(main_page.add_to_cart_button) is True
         main_page.open()
+
+
+def test_remove_all_items_from_cart(main_page):
+    """ verifies if items can be removed from a cart,
+        and the qty number on the cart badge displays nothing after all items were removed
+    """
+    main_page.open()
     main_page.open_the_cart()
     main_page.remove_all_items_in_cart()
     main_page.open()
